7c8e8ddd15719a234e03b162ad0e93355d2e38a1
liuliu <l.liu@sibionics.com@sibionics.com>
2022-01-10 18:08:55 +0800

    提取每次轮询的单个线圈的最常见的值作为该发射线圈的电流，并使用队列来存储和获取数据

--- readData.py
+++ readData.py
@@ -1,10 +1,13 @@
 import re
+import time
+from queue import Queue
+from multiprocessing.dummy import Process
 
 import serial
 import serial.tools.list_ports
 
 
-def readCurrent():
+def readCurrent(q):
     port = "COM3"
     ser = serial.Serial(port, 921600, timeout=0.5)
     if ser.isOpen():
@@ -20,15 +23,59 @@ def readCurrent():
         \xaaU\xeb\x90chD:000
         '''
         dataRe = re.findall(b'ch\w:\d{3}', data)
-        chCurrents = []
+        print('--time:{:.3f}--------data size={}---------'.format(time.time(),len(dataRe)))
+        chCurrents = {
+            'ch1': {},
+            'ch2': {},
+            'ch3': {},
+            'ch4': {},
+            'ch5': {},
+            'ch6': {},
+            'ch7': {},
+            'ch8': {},
+            'ch9': {},
+            'chA': {},
+            'chB': {},
+            'chC': {},
+            'chD': {},
+            'chE': {},
+            'chF': {},
+            'chG': {}
+        }
         for chc in dataRe:
             ch = str(chc[:3], encoding='utf-8')
             currentStr = str(chc[-3:], encoding='utf-8')
             current = int(currentStr[0]) + int(currentStr[1:3]) * 0.01
-            chCurrents.append({ch: current})
-        print(chCurrents)
-        print('----------data size={}---------'.format(len(chCurrents)))
+            if chCurrents[ch].get(current) == None:
+                chCurrents[ch][current] = 0
+            else:
+                chCurrents[ch][current] += 1
+        #print(chCurrents)
+        q.put(chCurrents)
+
+def getData(q):
+    '''
+    从队列中提取数据
+    '''
+    while True:
+        if not q.empty():
+            data = q.get()
+            for key in data.keys():
+                chCurrents = data.get(key)
+                currents = list(chCurrents.keys())
+                currentTmp = currents[0]
+                for i in range(len(currents) - 1):
+                    if chCurrents.get(currentTmp) < chCurrents.get(currents[i+1]):
+                        currentTmp = currents[i+1]
+                data[key] = currentTmp
+            print(data)
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
-    readCurrent()
+    q = Queue()
+    proReadData = Process(target=readCurrent, args=(q,))
+    proReadData.daemon = True
+    proReadData.start()
+
+    getData(q)
